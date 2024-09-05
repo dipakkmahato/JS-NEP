18c1e06c0271b0b8713e230e724f5ac4257a8faa
dipakkmahato <106677227+dipakkmahato@users.noreply.github.com>
2024-09-05 17:12:36 +0000

    variables information

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "dpmahato123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhaktpur"
+let accountState;
+// accountId = 2 //not allowed 
+console.log(accountId);
+
+/* 
+Prefer not use var
+because of issues in scope and functional scope
+*/
+
+
+accountEmail = "jastu@gmai;.com"
+accountPassword = "212121"
+accountCity = "Lalirpur"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ]);
+
