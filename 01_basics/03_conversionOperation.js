8d09fe41bf7856d2677c85e1447402c5812d862c
dipakkmahato <106677227+dipakkmahato@users.noreply.github.com>
2024-09-06 05:41:54 +0000

    conversuin of datatypes information

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-//let score = "dipak"
+// let score = "dipak"
 
 // console.log(typeof score);
 // console.log(typeof(score));
@@ -21,7 +21,30 @@
 // "" => false; "dipak" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+// let str1 = "dipak"
+// let str2 = " Singh"
+// let str = str1 + str2
+// console.log(str);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
 
